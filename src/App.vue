85feecaf2392bc4096a13b8b49ce754707711f8b
suchanun6104101393 <zadkee27@gmail.com>
2020-08-18 13:06:05 +0700

    18/08

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="logo game" src="./assets/logo1.png">
+    <HelloWorld msg="Welcome to Game"/>
   </div>
+ 
+  
+
+
 </template>
 
 <script>
